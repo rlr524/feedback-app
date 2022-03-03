deab2e4214939b1cdd6665c46e46ee99aae2654d
Rob Ranf <rob@emiyaconsulting.com>
2022-03-02 18:16:48 -0800

    misc

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,7 @@
 function App() {
-	const title = "Blog Post";
-	const body = "This is my blog post";
-	var comments = [
-		{ id: 1, text: "Comment 1" },
-		{ id: 2, text: "Comment 2" },
-		{ id: 3, text: "Comment 3" },
-	];
-
-	var loading = false;
-	var showComments = false;
-
-	const commentBlock = (
-		<div className="comments">
-			<h3>Comments ({comments.length})</h3>
-			<ul>
-				{comments.map((comment, index) => (
-					<li key={index}>{comment.text}</li>
-				))}
-			</ul>
-		</div>
-	);
-
-	const noCommentsBlock = (
-		<div>
-			<h3>No Comments Yet</h3>
-		</div>
-	);
-
-	if (loading) return <h1>Loading...</h1>;
-
 	return (
 		<div className="container">
-			<h1>{title.toUpperCase()}</h1>
-			<h2>{body}</h2>
-			<h2>{Math.random() * 5}</h2>
-			{showComments ? commentBlock : noCommentsBlock}
+			<h1>Madison's App</h1>
 		</div>
 	);
 }
